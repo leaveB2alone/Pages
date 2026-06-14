1de3ac1a55233faf08ca30e14ceef87ee1ba6ad6
leaveb2alone <leaveb2alone@gmail.com>
2026-06-14 18:35:24 -0300

    Criação do tema 'Evangelion' e desenvolvimento do menu inicial e guia 'About'

---
+++ JS/Evangelion.js
@@ -0,0 +1,29 @@
+function move(option){
+    const menu = document.querySelector('.menu');
+    const optionBack = document.querySelector('.options-back');
+    const screen = document.querySelectorAll('.screen');
+    menu.classList.add('hide');
+    screen.forEach(s => s.classList.remove('active'));
+    optionBack.classList.add('show');
+    if (option.id === 'about'){
+        document.querySelector('#screen-about').classList.add('active');
+    }
+}
+
+function moveback(option2){
+    const option = document.querySelector('.options-back');
+    const screen = document.querySelectorAll('.screen');
+    screen.forEach(s => s.classList.remove('active'));
+    if (option2.id === 'home2'){
+        document.querySelector('.menu').classList.remove('hide')
+        option.classList.remove('show')
+    }
+    if (option2.id === 'about2'){
+        window.alert('You are already in about :D');
+        document.querySelector('#screen-about').classList.add('active')
+    }
+}
+
+function home(){
+    window.alert('You are already in home :D');
+}
