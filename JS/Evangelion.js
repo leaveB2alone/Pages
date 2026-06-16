29d8be0c0a6d2e32012d04952e324710b8a8a924
leaveb2alone <leaveb2alone@gmail.com>
2026-06-16 17:57:03 -0300

    Tema 'Evangelion' finalizado, redirecionador finalizado

--- JS/Evangelion.js
+++ JS/Evangelion.js
@@ -1,4 +1,24 @@
+const music = document.getElementById("bg-music"); //---------------------------------Sons-----------------------------------------------------------
+
+document.body.addEventListener("click", () => {
+    music.play();
+}, { once: true });
+
+const hoversom1 = document.getElementById("hover-som1");
+const hoversom2 = document.getElementById("hover-som2");
+
+function playSom1() {
+    hoversom1.currentTime = 0;
+    hoversom1.play();
+}
+
+function playSom2() {
+    hoversom2.currentTime = 0;
+    hoversom2.play();
+}
+
 function move(option){
+    playSom2()
     const menu = document.querySelector('.menu');
     const optionBack = document.querySelector('.options-back');
     const screen = document.querySelectorAll('.screen');
@@ -12,9 +32,13 @@ function move(option){
     if (option.id === 'socials'){
         document.querySelector('#screen-socials').classList.add('active');
     }
+    if (option.id === 'projects'){
+        document.querySelector('#screen-projects').classList.add('active');
+    }
 }
 
 function moveback(option2){
+    playSom2()
     const option = document.querySelector('.options-back');
     const screen = document.querySelectorAll('.screen');
     screen.forEach(s => s.classList.remove('active'));
@@ -28,8 +52,19 @@ function moveback(option2){
     if (option2.id === 'socials2'){
         document.querySelector('#screen-socials').classList.add('active')
     }
+    if (option2.id === 'projects2'){
+        document.querySelector('#screen-projects').classList.add('active')
+    }
 }
 
 function home(){
     window.alert('You are already in home');
 }
+
+document.querySelector('#screen-socials').addEventListener('wheel', function(e) {
+    e.preventDefault();
+    this.scrollBy({
+        left: e.deltaY * 10,
+        behavior: 'smooth'
+    });
+}, { passive: false });
---
+++ Script.js
@@ -0,0 +1,7 @@
+const temas = [
+    'Celeste',
+    'Evangelion',
+];
+
+const aleatorio = Math.floor(Math.random() * temas.length);
+window.location.replace(`https://leaveb2alone.github.io/Pages/HTML/${temas[aleatorio]}.html`);
